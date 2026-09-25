41013775bc893123d287300ed638075110562849
Mohomed-Zaid <zaidn2848@gmai.com>
2026-09-25 14:04:57 +0530

    Done

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d90baac9'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jneanh50kk8"
+    "revision": "0.5pht3s20vb"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
